da4fd0855f6dba1220a0bb5b18f9d37cf05646c3
nnesmelov <n.nesmelov@dapsite.ru>
2023-07-06 17:51:56 +0300

    Создал основные файлы. Начал ветстать страницу. Шапка готова

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+$('.banner-slider').slick({
+    dots: false,
+    arrows: true,
+    infinite: false,
+    speed: 600,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+
+    ]
+  });
